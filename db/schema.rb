5332ba6b428bad1d0b3261cf5cd7da6954e4c1f8
samesame12 <d-m-play@windowslive.com>
2018-07-06 02:12:24 +0700

    3

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_07_05_184445) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "reports", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
